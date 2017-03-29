139d11be60d1b4108a1aa45c30928521915f17a7
busyrat <1435858847@qq.com>
2017-03-29 11:15:26 +0800

    新建脚手架

---
+++ RECORD.md
@@ -0,0 +1,16 @@
+#1. 下载vue-cli脚手架
+
+```$ vue init webpack resume```
+
+#2. 进入脚手架目录
+
+```$ cd resume```
+
+#3. 下载脚手架依赖包
+
+```$ npm install```
+
+#4. 使用stylus预编译，下载相关依赖包
+
+```$ npm install -D stylus stylus-loader```
+
---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+#app
+  text-align: center
+  color: #2c3e50
+  margin-top: 60px
+</style>
