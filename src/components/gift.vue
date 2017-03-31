848365a275df896cff1b09839fe2a05d2fefebf0
wh-Coder <461891475@qq.com>
2017-03-31 11:17:54 +0800

    测试了swiper和scroller

--- src/components/gift.vue
+++ src/components/gift.vue
@@ -2,42 +2,57 @@
   <div class="vue-gift">
     <ul class="tab">
       <li class="tab-receive" @click="onToggleReceive">
-        <span class="icon" :class="receiveUpDown?'icon-circle-up':'icon-circle-down'"></span>已收礼物(12)
+        <span class="icon" :class="isShowReceive?'icon-circle-up':'icon-circle-down'"></span>已收礼物(12)
       </li>
-      <li class="tab-present">
+      <li class="tab-present" @click="onTogglePresent">
         <span class="icon icon-gift"></span>
       </li>
     </ul>
-    <div class="present">
-      <ul class="present-title"></ul>
-      <swiper class="present-content" height="100%" dots-position="center">
-        <swiper-item>
-          <div class="test1"></div>
-        </swiper-item>
-        <swiper-item>
-          <div class="test2"></div>
-        </swiper-item>
-        <swiper-item>
-          <div class="test3"></div>
-        </swiper-item>
-      </swiper>
-      <ul class="present-footer"></ul>
-    </div>
+    <transition name="present">
+      <div class="present" v-if="isShowPresent1" v-show="isShowPresent2">
+        <scroller lock-y :scrollbar-x=false>
+          <div class="present-title">
+            <span class="title-item" v-for="(item,index) in 10">我是item{{index}}</span>
+          </div>
+        </scroller>
+        <swiper class="present-content" height="100%" dots-position="center">
+          <swiper-item>
+            <div class="test1"></div>
+          </swiper-item>
+          <swiper-item>
+            <div class="test2"></div>
+          </swiper-item>
+          <swiper-item>
+            <div class="test3"></div>
+          </swiper-item>
+        </swiper>
+        <ul class="present-footer"></ul>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  import {Swiper, SwiperItem} from 'vux'
+  import {Swiper, SwiperItem, Scroller} from 'vux'
   export default {
     data() {
       return {
-        receiveUpDown: true
+        isShowReceive: true,
+        isShowPresent1: false,
+        isShowPresent2: true
       };
     },
-    components: {Swiper, SwiperItem},
+    components: {Swiper, SwiperItem, Scroller},
     methods: {
       onToggleReceive(){
-        this.receiveUpDown = !this.receiveUpDown;
+        this.isShowReceive = !this.isShowReceive;
+      },
+      onTogglePresent(){
+        if (!this.isShowPresent1) {
+          this.isShowPresent1 = !this.isShowPresent1;
+        } else {
+          this.isShowPresent2 = !this.isShowPresent2;
+        }
       }
     }
   };
@@ -87,13 +102,18 @@
       width: 100%
       bottom: 0
       background: mediumvioletred
-      z-index: 2100;
+      z-index: 1900;
       .present-title
+        display:inline-block
         height: 40px;
         line-height: 40px;
         padding: 0 8px;
         white-space: nowrap;
         background darkolivegreen
+        .title-item
+          display:inline-block
+          margin: 4px 8px
+          background: #ccc
       .present-content
         background: blueviolet;
         padding: 10px 0;
@@ -115,4 +135,9 @@
         padding: 2px 16px;
         position: relative;
         height: 36px;
+    .present-enter-active, .present-leave-active
+      transition: all .5s ease
+      transform: translate3d(0, 0, 0)
+    .present-enter, .present-leave-active
+      transform: translate3d(0, 100%, 0)
 </style>
