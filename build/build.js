84e11e00e596cd4bd576c845a84b1c595c6b60f4
wh_Coder <461891475@qq.com>
2017-05-08 17:44:05 +0800

    git

--- build/build.js
+++ build/build.js
@@ -29,7 +29,8 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
     console.log(chalk.cyan('  Build complete.\n'))
     console.log(chalk.yellow(
       '  Tip: built files are meant to be served over an HTTP server.\n' +
-      '  Opening index.html over file:// won\'t work.\n'
+      '  Opening index.html over file:// won\'t work.\n' +
+      '  https://rawgit.com/wh-Coder/resume/master/dist/index.html \n '
     ))
   })
 })
